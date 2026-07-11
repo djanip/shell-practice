ff1ab0e794b5d718e17024a38b0d8ca82c66a239
divya <divyajanipalli.com>
2026-07-11 16:11:23 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2 
+
+echo "Trump:: Welcome to Alaska Putin"
+echo "Putin:: Thank you Trump, finally not on Zoom Webinar"
+echo "Trump:: So, do we have a deal"
+echo "Putin:: Yes, if you make Ukraine joins Russia"
