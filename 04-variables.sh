34e5d083dda6ecd229643a789123ecb54f266f61
divya <divyajanipalli.com>
2026-07-11 16:17:26 +0530

    variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2 
 
-echo "Trump:: Welcome to Alaska Putin"
-echo "Putin:: Thank you Trump, finally not on Zoom Webinar"
-echo "Trump:: So, do we have a deal"
-echo "Putin:: Yes, if you make Ukraine joins Russia"
+echo "PERSON1:: Welcome to Alaska PERSON2"
+echo "PERSON2:: Thank you PERSON1, finally not on Zoom Webinar"
+echo "PERSON1:: So, do we have a deal"
+echo "PERSON2:: Yes, if you make Ukraine joins Russia"
