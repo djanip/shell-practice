13a3532907a3b98d2f24dda02afd9b59e4b18ab1
divya <divyajanipalli.com>
2026-07-12 18:50:12 +0530

    variables

---
+++ 12-installation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Error:: Installing MySql is failure"
+    exit 1
+else
+    echo "Installing MySql is SUCCESS"
+fi
+
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Error:: Installing Nginx is failure"
+    exit 1
+else
+    echo "Installing Nginx is SUCCESS"
+fi
+
+dnf install mongodb-mongosh -y
+
+if [ $? -ne 0 ]; then
+    echo "Error:: Installing mongodb-mongosh is failure"
+    exit 1
+else
+    echo "Installing mongodb-mongosh is SUCCESS"
+fi
+
