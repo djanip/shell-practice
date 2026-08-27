b51da0e9eb025711081324eb4bb4cb70ee86560d
divya <divyajanipalli.com>
2026-08-27 11:05:59 +0530

    variables

--- 18-loops.sh
+++ 18-loops.sh
@@ -37,8 +37,8 @@ do
     # if exit status is 0, already installed. -ne 0 need to install it
     if [ $? -ne 0 ]; then
         dnf install $package -y &>>$LOG_FILE
-        VALIDATE $? "package"
+        VALIDATE $? "$package"
     else
         echo -e "$package already installed ... $Y SKIPPING $N"
     fi        
-done
+done 
