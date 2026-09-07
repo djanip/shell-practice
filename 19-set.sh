1eb08b3caa8ec06e0d2b3407fa638125c8b29a54
divya <divyajanipalli.com>
2026-09-07 12:43:58 +0530

    variables

--- 19-set.sh
+++ 19-set.sh
@@ -2,7 +2,13 @@
 
   set -e
 
+  error(){
+      echo "There is an error"
+  }
+
+  trap error ERR
+
   echo "Hello.."
   echo "Before error.."
-  ccaffjl;dnf
+  ccaffjl;dnf # here shell understands there is an error and signal is ERR
   echo "After error"
